91215b5201c520516c0fbbd4f2c55afe9b512c0f
cleristonlincoln <cleristonlincoln@hotmail.com>
2021-10-22 14:54:43 -0300

    implementando listar transferencias tipo empresa

--- src/main/java/br/com/somapay/api/controller/ContaController.java
+++ src/main/java/br/com/somapay/api/controller/ContaController.java
@@ -1,6 +1,8 @@
 package br.com.somapay.api.controller;
 
+import br.com.somapay.domain.model.ContaEmpresa;
 import br.com.somapay.domain.model.ContaFuncionario;
+import br.com.somapay.domain.repository.ContaEmpresaRepository;
 import br.com.somapay.domain.repository.ContaFuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,21 @@ import java.util.List;
 @RequestMapping("conta")
 public class ContaController {
 
-@Autowired
+    @Autowired
     private ContaFuncionarioRepository contaFuncionarioRepository;
 
+    @Autowired
+    private ContaEmpresaRepository contaEmpresaRepository;
 
-@GetMapping("funcionario")
-    public List<ContaFuncionario> contasFuncionario(){
-    return contaFuncionarioRepository.findAll();
-}
+
+    @GetMapping("funcionario")
+    public List<ContaFuncionario> contasFuncionario() {
+        return contaFuncionarioRepository.findAll();
+    }
+
+    @GetMapping("empresa")
+    public List<ContaEmpresa> contasEmpresa() {
+        return contaEmpresaRepository.findAll();
+    }
+
 }
